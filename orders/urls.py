4f291d07d26ce107a0ae297593b3f585064bbf8b
dart-wakar <wakar@whitelogic.in>
2017-04-09 01:02:11 +0530

    Create order and edit order views

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,6 @@ from orders import views
 urlpatterns = [
     url(r'^orders/$',views.OrdersList.as_view()),
     url(r'^orders/(?P<pk>[0-9]+)/$',views.OrderDetail.as_view()),
+    url(r'^orders/create/$',views.OrderCreate.as_view()),
+    url(r'^orders/edit/$',views.OrderEdit.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,10 @@
 from orders.models import Orders
 from orders.serializers import OrderSerializer
 from rest_framework import generics
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -11,3 +15,23 @@ class OrdersList(generics.ListAPIView):
 class OrderDetail(generics.RetrieveAPIView):
     queryset = Orders.objects.all()
     serializer_class = OrderSerializer
+
+class OrderCreate(generics.CreateAPIView):
+    queryset = Orders.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderEdit(APIView):
+    def get_order(self,pk):
+        try:
+            return Orders.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            raise Http404
+
+    def post(self,request,format=None):
+        pk = request.data['id']
+        order = self.get_order(pk)
+        serializer = OrderSerializer(order,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
