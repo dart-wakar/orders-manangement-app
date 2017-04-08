030475cf9b87bf65e53ba5aef61a35df2012a5a8
dart-wakar <wakar@whitelogic.in>
2017-04-08 12:45:38 +0530

    Order model created

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,17 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+class Orders(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100,blank=True)
+    website_name = models.CharField(max_length=100,blank=True)
+    expected_delivery_date = models.DateField(blank=True)
+    status = models.IntegerField(default=0)
+    delivered_date = models.DateTimeField(blank=True)
+
+    class Meta:
+        ordering = ('created',)
