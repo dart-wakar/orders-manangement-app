627da7ba636b6d8210001ba1b9be122f9aa4baa4
dart-wakar <wakar@whitelogic.in>
2017-04-08 13:54:07 +0530

    Model based Order Serializer

--- orders/models.py
+++ orders/models.py
@@ -9,9 +9,9 @@ class Orders(models.Model):
     modified = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=100,blank=True)
     website_name = models.CharField(max_length=100,blank=True)
-    expected_delivery_date = models.DateField(blank=True)
+    expected_delivery_date = models.DateField(blank=True,null=True)
     status = models.IntegerField(default=0)
-    delivered_date = models.DateTimeField(blank=True)
+    delivered_date = models.DateTimeField(blank=True,null=True)
 
     class Meta:
         ordering = ('created',)
